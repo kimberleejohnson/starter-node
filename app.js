d6ad251de10ce6a89298e9b333c8c4ac5da68291
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-03-27 08:11:49 -0700

    Added body parser module in routes

--- app.js
+++ app.js
@@ -32,16 +32,17 @@ app.get('/', function(req, res, next) {
 
 // handle a POST request to send a text message. 
 // This is sent via ajax on our home page
-app.post('/message', function(req, res, next) {
-  // Use the REST client to send a text message
-  client.messages.create({
-    to: req.body.to,
-    from: TWILIO_PHONE_NUMBER,
-    body: 'Good luck on your Twilio quest!'
-  }).then(function(message) {
-    // When we get a response from Twilio, respond to the HTTP POST request
-    res.send('Message is inbound!');
-  });
+app.post('/sms', (request, response) => {
+  console.log(
+    `Incoming message from ${request.body.From}: ${request.body.Body}`
+  );
+  // Here, we're writing and returning raw TwiML
+  response.type('text/xml');
+  response.send(`
+    <Response>
+      <Message>I'm busy questing right now!</Message>
+    </Response>
+  `);
 });
 
 // handle a POST request to make an outbound call.
